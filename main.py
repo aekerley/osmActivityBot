921c3a1a166eaaa328badd81b219f808fe42896b
Aleksey Grechko <leshka756@gmail.com>
2023-10-15 01:18:11 +0500

    Добавлена рекурсия в обработчик загрузки фотографии. Добавленны поля для учета потраченынх баллов.

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def initialize_database():
             telegram_id INTEGER,
             first_name TEXT,
             last_name TEXT,
-            registration_date DATE
-            total_points INTEGER
-            spent_points INTEGER
+            registration_date DATE,
+            total_points INTEGER,
+            spent_points INTEGER DEFAULT 0
         )
     ''')
 
@@ -45,6 +45,26 @@ def initialize_database():
         )
     ''')
 
+    # Создаем таблицу store
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS store (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            price REAL,
+            stock INTEGER
+        )
+    ''')
+
+    # Создаем таблицу purchases
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS purchases (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            product_id INTEGER,
+            purchase_date DATE,
+            quantity INTEGER
+        )
+    ''')
     conn.commit()
     conn.close()
 
@@ -133,6 +153,7 @@ def show_main_menu(chat_id, is_registered=True):
     if is_registered:
         markup.row(types.KeyboardButton("Добавить активность⚡"))
         markup.row(types.KeyboardButton("Посмотреть активности"), types.KeyboardButton("Баллы"))
+        markup.row(types.KeyboardButton("Магазин"))
         markup.row(types.KeyboardButton("Инструкция"))
     else:
         markup.add(types.KeyboardButton("Инструкция"))
@@ -152,7 +173,11 @@ def handle_buttons(message):
         view_activities(message)
     elif text == "Баллы":
         view_points(message)
+    elif text == "Магазин":
+        show_store(message)
     elif text == "/start":
+        show_main_menu(chat_id, is_registered=True)
+    elif text == "Отмена":
         show_main_menu(chat_id, is_registered=True)
 
 # Функция для отправки списка категорий активностей
@@ -335,15 +360,126 @@ def view_points(message):
     cursor.execute("SELECT SUM(points) FROM activities WHERE telegram_id=?", (user_id,))
     total_points = cursor.fetchone()[0]
 
+    cursor.execute("SELECT spent_points FROM users WHERE telegram_id=?", (user_id,))
+    spent_points = cursor.fetchone()[0]
+
     conn.close()
 
     if total_points is not None:
-        bot.send_message(chat_id, f"Ваши баллы: {total_points}\n*Баллы обновляются 1 числа каждого месяца!")
+        available_points = total_points - spent_points
+        bot.send_message(chat_id, f"Ваши баллы: {available_points}\n*Баллы обновляются 1 числа каждого месяца!")
     else:
         bot.send_message(chat_id, "У вас пока нет баллов.\n*Баллы обновляются 1 числа каждого месяца!", parse_mode='html')
 
+@bot.message_handler(func=lambda message: message.text == "Магазин")
+def show_store(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Вы находитесь в разделе 'Магазин'.", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton("Отмена")))
+
+    # Получите список товаров из таблицы store
+    conn = sqlite3.connect('database_aleksey.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name, price, stock FROM store")
+    products = cursor.fetchall()
+    conn.close()
+
+    if not products:
+        bot.send_message(chat_id, "В магазине нет доступных товаров.")
+    else:
+        store_info = "Магазин:\n"
+        keyboard = types.InlineKeyboardMarkup(row_width=2)
+
+        for product in products:
+            product_id, name, price, stock = product
+            item_text = f"{name} - ${price} (В наличии: {stock} шт.)"
+            callback_data = f"buy_{product_id}"
+
+            button = types.InlineKeyboardButton(item_text, callback_data=callback_data)
+            keyboard.add(button)
+
+        bot.send_message(chat_id, store_info, reply_markup=keyboard)
+        bot.send_message(chat_id, "Выберите товар или введите другую команду.")
+
+# Обработчик для инлайн-кнопок товаров
+@bot.callback_query_handler(func=lambda call: call.data.startswith("buy_"))
+def handle_buy_callback(call):
+    product_id = int(call.data.replace("buy_", ""))
+
+    # Получите информацию о товаре из таблицы store
+    conn = sqlite3.connect('database_aleksey.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, price, stock FROM store WHERE id=?", (product_id,))
+    product_info = cursor.fetchone()
+    conn.close()
+
+    if product_info:
+        name, price, stock = product_info
+        message_text = f"{name} - ${price}\nВ наличии: {stock} шт."
+
+        # Получите путь к изображению товара
+        images_path = 'img/'  # Укажите путь к папке с изображениями
+        image_filename = os.path.join(images_path, f"{product_id}.png")
+
+        # Проверяем, существует ли файл изображения
+        if os.path.isfile(image_filename):
+            # Отправляем изображение товара с подписью и инлайн-клавиатурой
+            with open(image_filename, 'rb') as image_file:
+                bot.send_photo(call.message.chat.id, image_file, caption=message_text, reply_markup=create_confirmation_keyboard(product_id))
+        else:
+            bot.send_message(call.message.chat.id, message_text, reply_markup=create_confirmation_keyboard(product_id))
+    else:
+        bot.send_message(call.message.chat.id, "Информация о товаре не найдена.")
+
+# Функция для создания инлайн-клавиатуры "Купить"
+def create_confirmation_keyboard(product_id):
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    buy_button = types.InlineKeyboardButton("Купить", callback_data=f"confirm_buy_{product_id}")
+    keyboard.add(buy_button)
+    return keyboard
+
+# Обработчик для инлайн-кнопок "Купить"
+@bot.callback_query_handler(func=lambda call: call.data.startswith("confirm_buy_"))
+def handle_buy_button_callback(call):
+    product_id = int(call.data.replace("confirm_buy_", ""))
+
+    # Здесь вы можете добавить логику покупки товара
+    increase_spent_points(call.message.chat.id, product_id)
+    # Уменьшаем количество товара на складе
+    decrease_stock(product_id)
+
+    bot.send_message(call.message.chat.id, f"Вы успешно купили товар с ID {product_id}. Сумма покупки добавлена в spent_points.")
+
+def decrease_stock(product_id):
+    conn = sqlite3.connect('database_aleksey.db')
+    cursor = conn.cursor()
+
+    # Уменьшаем количество товара на складе (stock) на 1
+    cursor.execute("UPDATE store SET stock = stock - 1 WHERE id = ?", (product_id,))
+
+    conn.commit()
+    conn.close()
+
+
+# Функция для увеличения суммы потраченных средств
+
+def increase_spent_points(user_id, product_id):
+    conn = sqlite3.connect('database_aleksey.db')
+    cursor = conn.cursor()
+
+    # Здесь предполагается, что у вас есть таблица "users" с полем "spent_points" для отслеживания потраченных средств
+    cursor.execute("UPDATE users SET spent_points = spent_points + (SELECT price FROM store WHERE id=?) WHERE telegram_id = ?", (product_id, user_id))
+
+    conn.commit()
+    conn.close()
+
+@bot.message_handler(commands=['Отмена'])
+def cancel(message):
+    chat_id = message.chat.id
+    show_main_menu(chat_id, is_registered=True)
+
 # Глобальные переменные
 user_states = {}
+images_path = "img"
 activity_categories = ["Участие в молодёжном мероприятии", "Организация мероприятия для молодёжи", "Выступление спикером или лектором на мероприятии ОСМ", "Получение наград за работу в молодежных сообществах", "Получение наград за продвижение молодежных инициатив", "Получение наград за реализацию проектов"]
 activity_subcategories = {
     "Участие в молодёжном мероприятии": ["Мероприятие уровня предприятия", "Мероприятие уровня дивизиона", "Мероприятие уровня отрасли"],
